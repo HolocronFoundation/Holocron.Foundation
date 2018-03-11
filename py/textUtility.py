8c016b5c2726107a7441313c17c82383c52b9099
SamuelTroper <samuel.troper@holocron.foundation>
2018-03-11 17:29:13 -0400

    Added Selection Criteria
    Filters files down to actual documents (no readmes, etc.).
    Also selects the version to keep in this order: ASCII > UTF-8 > Latin-1

--- py/textUtility.py
+++ py/textUtility.py
@@ -2,12 +2,45 @@ import os
 from shutil import copy2
 from fnmatch import fnmatch
 
-def moveTexts(directory='C:/gutenberg/', newDirectory='C:/gutenbergNoSubs/'):
+def selectTexts(directory='C:/gutenberg/'):
+    fileDict = {}
+    nameList = []
     for path, subdirs, files in os.walk(directory):
         for name in files:
             if fnmatch(name, '*.txt'):
-                print(os.path.join(path, name))
                 #Easy exception: if filename contains non-numerical characters
-                #                besides -, then skip. Not yet implemented for
-                #                experimental reasons.
-                copy2(os.path.join(path, name), newDirectory)
+                #                besides -, then skip.
+                stripName = name.replace('.txt', '').replace('-', '')
+                if stripName.isdigit() and 'old' not in os.path.join(path, name):
+                    fileDict[name] = os.path.join(path, name)
+                    nameList.append(name)
+                    print(fileDict[name])
+
+    #Removes multiple copies of the same text. Keeps the simplest format.
+    nameList.sort()
+    finalList = []
+    for name in nameList:
+        #Adds ASCII
+        if '-' not in name:
+            finalList.append(name)
+        #Checks if ASCII will be added
+        elif (name.replace('-8', '') not in nameList) or (name.replace('-0', '') not in nameList):
+            #If UTF-8, then add
+            if '-0' in name:
+                finalList.append(name)
+            #If Latin-1, and UTF-8 will NOT be added, add Latin-1
+            elif name.replace('-8', '-0') not in nameList:
+                finalList.append(name)
+    fileList = []
+    for name in finalList:
+        fileList.append(fileDict[name])
+    return fileList
+            
+                    
+
+def moveTexts(directory='C:/gutenberg/', newDirectory='C:/gutenbergNoSubs/'):
+    for file in selectTexts(directory):
+        copy2(file, newDirectory)
+
+#Future to do:
+        #Add Big-5 support. Should be simple, similar to UTF and Latin... No books are in that format anyway yet...
