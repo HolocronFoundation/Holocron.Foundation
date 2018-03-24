5637d863a1069e5e742b4806b3747632053d2386
SamuelTroper <samuel.troper@holocron.foundation>
2018-03-24 14:59:56 -0400

    Further compilation changes

--- vyper/bookContract.v.py
+++ vyper/bookContract.v.py
@@ -16,49 +16,49 @@ BookUploaded: event({_bookID: int128})
 foundationAddresses: public(address[3])
 
 #foundationMultiplier and foundationDivisor are used to fractionalize donations without decimals
-foundationMultiplier: public(int128)
-foundationDivisor: public(int128)
+foundationSplit: public(decimal)
 
 #Update address - Address for an updated contract, to allow for patches.
 updateAddress: public(address)
 
 #book struct: This struct will contain all the info for a given book
 book: public({
-    title: public(bytes<=10), #NEEDS GENERATED
-    USPublicDomain: public(bool),
-    language: public(bytes <= 2),
-    libraryOfCongress: public(bytes),
-    subjects: public(bytes),
-    authorIDs: public(int128[int128]),
-    authorRoles: public(int128[int128]),
-    size: public(int128),
-    donations: public(wei_value),
+    title: bytes <= 10,   #NEEDS FIXED
+    USPublicDomain: bool,
+    language: bytes <= 2,
+    libraryOfCongress: bytes <= 10,    #NEEDS FIXED
+    subjects: bytes <= 10,     #NEEDS FIXED
+    authorIDs: int128[3],    #NEED TO CHECK LENGTH NEEDED
+    authorRoles: int128[3],      #NEED TO CHECK LENGTH NEEDED
+    size: int128,
+    donations: wei_value,
     textAddress: address,
     uploaded: bool
 }[int128])
+#Need to add method to add addional authors
 
 #Initiation
 @public
-def __init__(_foundationAddresses: address[3], _foundationMultiplier: int128, _foundationDivisor: int128):
-    foundationAddresses = _foundationAddresses
-    foundationMultiplier = _foundationMultiplier
-    foundationDivisor = _foundationDivisor
+def __init__(_foundationAddresses: address[3], _foundationSplit: decimal):
+    self.foundationAddresses = _foundationAddresses
+    self.foundationSplit = _foundationSplit
 
 #Address Array - Authorized Contract Editors
     #3 addresses to allow for editing and backups
     #First address recieves ETH immeadiately
 @public
 def changeFoundationAddresses(index: int128, newAddress: address):
-    assert msg.sender in foundationAddresses and index >= 0 and index <= 3
+    assert msg.sender == self.foundationAddresses[0]
+    assert index >= 0
     self.foundationAddresses[index] = newAddress
 
 #donate - splits funds recieved between the foundation and an individual text
 @payable
 @public
 def donate(id: int128):
-    split: wei_value = msg.value * self.foundationMultiplier / self.foundationDivisor
+    split: wei_value = as_wei_value(msg.value * self.foundationSplit, "wei")
     send(self.foundationAddresses[0], split)
-    self.book[id].donations += msg.value - split
+    self.book[id].donations += as_wei_value(msg.value, "wei") - split
     log.Donation(msg.sender, msg.value)
 
 @public
@@ -66,9 +66,11 @@ def setUpdateAddress(newUpdateAddress: address):
     assert msg.sender == self.foundationAddresses[0]
     self.updateAddress = newUpdateAddress
 
-@public
-def AddBook(id: int128, _title: bytes, _USPublicDomain: bool, _language: bytes <= 2, _libraryOfCongress: bytes, _subjects: bytes, _authorIDs: int128[int128], _authorRoles: int128[int128], _size: int128):
+@public                 #NEEDS FIXED                                                        N           EEDS FIXED                  NEEDS FIXEd             Needs fixed     Needs fixed
+def AddBook(id: int128, _title: bytes  <= 10, _USPublicDomain: bool, _language: bytes <= 2, _libraryOfCongress: bytes <= 10, _subjects: bytes <= 10, _authorID: int128, _authorRole: int128, _size: int128):
     assert msg.sender == self.foundationAddresses[0]
+    _authorRoles: int128[3]
+    _authorIDs: int128[3]
     self.book[id] = {
         title: _title,
         USPublicDomain: _USPublicDomain,
@@ -76,10 +78,11 @@ def AddBook(id: int128, _title: bytes, _USPublicDomain: bool, _language: bytes <
         libraryOfCongress: _libraryOfCongress,
         subjects: _subjects,
         authorIDs: _authorIDs,
-        authorRoles: _authorsRoles,
+        authorRoles: _authorRoles,
         size: _size,
         donations: 0,
-        textAddress: 0
+        textAddress: self.foundationAddresses[0],
+        uploaded: False
     }
     log.BookUploaded(id)
     
